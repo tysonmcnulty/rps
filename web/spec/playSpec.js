c52dccbf6053a511d67d9cbaf69e0f4d9088662f
Matthew Kane Parker <mparker@pivotallabs.com>
2017-04-06 09:41:08 -0400

    TIE and WIN scenarios handled on frontend

--- web/spec/playSpec.js
+++ web/spec/playSpec.js
@@ -16,6 +16,14 @@ const RpsApp = React.createClass({
         this.setState({message: "INVALID"})
     },
 
+    tie(){
+        this.setState({message: "TIE"})
+    },
+
+    winner(player){
+        this.setState({message: `${player.toUpperCase()} WINS`})
+    },
+
     render(){
         return (
             <div>
@@ -40,6 +48,54 @@ describe("play", function () {
             submitPlayForm()
 
             expect(page()).toContain("INVALID")
+        })
+    })
+
+    describe("tie", function () {
+        beforeEach(function () {
+            renderApp({
+                play: function(p1, p2, ui){
+                    ui.tie()
+                }
+            })
+        })
+
+        it("displays TIE to the user", function () {
+            submitPlayForm()
+
+            expect(page()).toContain("TIE")
+        })
+    })
+
+    describe("p1 wins", function () {
+        beforeEach(function () {
+            renderApp({
+                play: function(p1, p2, ui){
+                    ui.winner("p1")
+                }
+            })
+        })
+
+        it("displays P1 WINS to the user", function () {
+            submitPlayForm()
+
+            expect(page()).toContain("P1 WINS")
+        })
+    })
+
+    describe("p2 wins", function () {
+        beforeEach(function () {
+            renderApp({
+                play: function(p1, p2, ui){
+                    ui.winner("p2")
+                }
+            })
+        })
+
+        it("displays P2 WINS to the user", function () {
+            submitPlayForm()
+
+            expect(page()).toContain("P2 WINS")
         })
     })
 
